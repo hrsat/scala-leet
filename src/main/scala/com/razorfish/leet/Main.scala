1369d85dbc9387298d300b1558fe716fce5a7328
hamid <hrsat110@hotmail.com>
2016-08-19 19:36:33 -0400

    include internal trait implementation

--- src/main/scala/com/razorfish/leet/Main.scala
+++ src/main/scala/com/razorfish/leet/Main.scala
@@ -33,8 +33,32 @@ object MyLeet extends Leetable[String] {
 
 object Main {
   def main(args: Array[String]): Unit = {
-    // add your implementation below this line
-    implicit val implicVal = MyLeet
+    /* add your implementation below this line
+
+     Two ways to have implicit Leetable: extend it externaly as separate object  */
+    //implicit val implicVal = MyLeet
+
+    //or internaly    while  creating object
+    implicit val implicVal= new Leetable[String] {
+      override def toLeet(in: String): String = {
+
+        def replaceChars(x: Char): String = x match {
+          case 'o' | 'O' => "0"
+          case 'i' | 'I' => "1"
+          case 'e' | 'E' => "3"
+          case 'a' | 'A' => "4"
+          case 's' | 'S' => "5"
+          case 't' | 'T' => "7"
+
+          case c => s"$c"
+        }
+
+        in flatMap {
+          replaceChars
+        }
+
+      }
+    }
 
     println(args(0)+" -> "+Leet.apply(args(0)))
 
